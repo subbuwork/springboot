8fd621e7f1fb82f298fa415f91ac2ec7fa44352f
subbu <subbu@192.168.0.233>
2017-04-07 13:06:08 -0400

    JMS with actvie mq and spring boot initial committ

---
+++ springboot-avtivemq/src/main/java/com/spring/boot/SpringbootAvtivemqApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAvtivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAvtivemqApplication.class, args);
+	}
+}
