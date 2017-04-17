6cadba9968e7faca9ced49ddf1cfd6a9c98f8be6
subbu <subbu@192.168.0.233>
2017-04-17 10:21:19 -0400

    Rest api consumer using spring rest template..

---
+++ SpringbootRestConsumer/src/main/java/com/spring/boot/rest/consumer/SpringbootRestConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot.rest.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestConsumerApplication.class, args);
+	}
+}
